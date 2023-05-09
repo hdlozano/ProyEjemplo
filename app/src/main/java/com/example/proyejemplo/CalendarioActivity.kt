f7a5e721d1d90fea3aa3b94e9b8304dbc97f7d1c
hdlozano <75293050+hdlozano@users.noreply.github.com>
2023-05-08 20:10:49 -0500

    Creacion de listado de citas

---
+++ app/src/main/java/com/example/proyejemplo/CalendarioActivity.kt
@@ -0,0 +1,22 @@
+package com.example.proyejemplo
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import com.google.firebase.database.FirebaseDatabase
+
+class CalendarioActivity : AppCompatActivity() {
+    private val db = FirebaseDatabase.getInstance()
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendario)
+        val layautVertical : LinearLayout = findViewById(R.id.layaut1)
+        val ref = db.getReference("Citas")
+
+        
+
+    }
+}
